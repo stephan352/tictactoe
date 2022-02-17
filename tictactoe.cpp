edfb64c02cf3278edc5cc01b78221d88276d34bf
Stephan <stephanhd0@gmail.com>
2022-02-17 11:15:48 +0700

    created board

---
+++ tictactoe.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// class Game {
+//     public:
+// };
+
+const char *board = " o | o | o\n\
+-----------\n\
+ o | o | o\n\
+-----------\n\
+ o | o | o\n";
+
+int main() {
+    cout << "Tic-Tac-Toe" << endl;
+    cout << board;
+    return 0;
+}
