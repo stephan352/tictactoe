2a221c29077e6cacfea9fdb6f03b695c05cfddf9
Stephan <stephanhd0@gmail.com>
2022-02-19 08:01:36 +0700

    added method to display the board

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,18 +1,27 @@
 #include <iostream>
 using namespace std;
 
-// class Game {
-//     public:
-// };
+class Game {
+    public:
+        string line = "-----------";
+        string row1 = " 1 | 2 | 3 ";
+        string row2 = " 4 | 5 | 6 ";
+        string row3 = " 7 | 8 | 9 ";
 
-const char *board = " o | o | o\n\
------------\n\
- o | o | o\n\
------------\n\
- o | o | o\n";
+        void display_board() {
+            cout<<row1<<endl<<line<<endl<<row2<<endl<<line<<endl<<row3;
+        };
+
+        void update_display(string input) {
+            // if (input == "1" || input == "2" || input == "3") {
+            //     row1.
+            // }
+        }
+};
 
 int main() {
     cout << "Tic-Tac-Toe" << endl;
-    cout << board;
+    Game game;
+    game.display_board();
     return 0;
 }
