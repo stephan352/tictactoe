c71905462a12023e7f974343790590e7f519a567
Stephan <stephanhd0@gmail.com>
2022-02-19 08:18:19 +0700

    added function to update display of first row

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -7,21 +7,25 @@ class Game {
         string row1 = " 1 | 2 | 3 ";
         string row2 = " 4 | 5 | 6 ";
         string row3 = " 7 | 8 | 9 ";
+        char turn = 'x';
 
         void display_board() {
-            cout<<row1<<endl<<line<<endl<<row2<<endl<<line<<endl<<row3;
+            cout<<row1<<endl<<line<<endl<<row2<<endl<<line<<endl<<row3<<endl;
         };
 
         void update_display(string input) {
-            // if (input == "1" || input == "2" || input == "3") {
-            //     row1.
-            // }
-        }
+            if (input == "1" || input == "2" || input == "3") {
+                int index = row1.find(input);
+                row1[index] = turn;
+            };
+        };
 };
 
 int main() {
     cout << "Tic-Tac-Toe" << endl;
     Game game;
+    game.display_board();
+    game.update_display("1");
     game.display_board();
     return 0;
 }
